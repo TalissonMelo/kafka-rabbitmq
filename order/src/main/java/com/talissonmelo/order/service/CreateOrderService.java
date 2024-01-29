56eebe165b328c4b7de006b27be45dafc4a1ffa2
Talisson Melo <talis.melo@petize.com.br>
2024-01-29 07:01:14 -0300

    Adicionando Actuator em order

--- order/src/main/java/com/talissonmelo/order/service/CreateOrderService.java
+++ order/src/main/java/com/talissonmelo/order/service/CreateOrderService.java
@@ -6,8 +6,10 @@ import com.talissonmelo.order.entities.response.OrderResponse;
 import com.talissonmelo.order.kafka.KafkaProducerMessage;
 import com.talissonmelo.order.repositories.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CreateOrderService {
@@ -16,6 +18,7 @@ public class CreateOrderService {
     private final KafkaProducerMessage kafkaProducerMessage;
 
     public OrderResponse execute(OrderRequest request) {
+        log.info("Create de Order");
         Order order = Order.createOrder(request.title(), request.description());
         OrderResponse response = toOrderResponse(repository.save(order));
         kafkaProducerMessage.sendMessage(response);
